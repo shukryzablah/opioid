002e678dd670af1ae9d519cef8bea073fdbc6263
shukryzablah <shukryzablah@gmail.com>
2019-04-15 17:26:03 -0400

    added new server and ui file

--- server.R
+++ server.R
@@ -12,52 +12,43 @@ load("PS.RData", envir = .GlobalEnv)
 
 function(input, output, session){
   
-  observeEvent(input$button1, {
-    updateTabsetPanel(session, "inTabset", selected = "tab3")
-  })
-  
-  observeEvent(input$button2, {
-    updateTabsetPanel(session, "inTabset", selected = "tab4")
-  })
-  
-  observeEvent(input$button3, {
-    updateTabsetPanel(session, "inTabset", selected = "tab1")
-  })
-  
-  output$plot1 <- renderPlotly({
-    if(input$select=="Age"){
-      ageGroupsData %>%
-        group_by(ageGroups, Year) %>%
-        filter(drugAlcoholInducedCause == "Drug poisonings (overdose) Unintentional (X40-X44)"|
-                 drugAlcoholInducedCause == "All other drug-induced causes"|
-                 drugAlcoholInducedCause == "Drug poisonings (overdose) Suicide (X60-X64)"|
-                 drugAlcoholInducedCause == "Drug poisonings (overdose) Undetermined (Y10-Y14)") %>%
-        summarize(numObs = n(), TotalDeaths = sum(Deaths)) %>%
-        plot_ly(x = ~Year, y = ~TotalDeaths, type = 'scatter', color = ~ageGroups, mode = "lines+markers")}
-    else if(input$select=="Race"){
-      raceData %>%
-        group_by(race, Year) %>%
-        filter(drugAlcoholInducedCause == "Drug poisonings (overdose) Unintentional (X40-X44)"|
-                 drugAlcoholInducedCause == "All other drug-induced causes"|
-                 drugAlcoholInducedCause == "Drug poisonings (overdose) Suicide (X60-X64)"|
-                 drugAlcoholInducedCause == "Drug poisonings (overdose) Undetermined (Y10-Y14)") %>%
-        summarize(numObs = n(), TotalDeaths = sum(Deaths)) %>%
-        plot_ly(x = ~Year, y = ~TotalDeaths, type = 'scatter', color = ~race, mode = "lines+markers")}
-    
-    else{
-      ggplotly(genderData %>%
-                 group_by(gender, Year) %>%
-                 filter(drugAlcoholInducedCause == "Drug poisonings (overdose) Unintentional (X40-X44)"|
-                          drugAlcoholInducedCause == "All other drug-induced causes"|
-                          drugAlcoholInducedCause == "Drug poisonings (overdose) Suicide (X60-X64)"|
-                          drugAlcoholInducedCause == "Drug poisonings (overdose) Undetermined (Y10-Y14)") %>%
-                 summarize(numObs = n(), TotalDeaths = sum(Deaths)) %>%
-                 plot_ly(x = ~Year, y = ~TotalDeaths, type = 'scatter', color = ~gender, mode = "lines+markers"))}
-  })
-  
   observeEvent(input$toTab2, {
     updateTabsetPanel(session, "titleHead",
                       selected = "tab2")
+  })
+  
+  
+  output$plot1 <- renderPlotly({
+    ageGroupsData %>%
+      group_by(ageGroups, Year) %>%
+      filter(drugAlcoholInducedCause == "Drug poisonings (overdose) Unintentional (X40-X44)"|
+               drugAlcoholInducedCause == "All other drug-induced causes"|
+               drugAlcoholInducedCause == "Drug poisonings (overdose) Suicide (X60-X64)"|
+               drugAlcoholInducedCause == "Drug poisonings (overdose) Undetermined (Y10-Y14)") %>%
+      summarize(numObs = n(), TotalDeaths = sum(Deaths)) %>%
+      plot_ly(x = ~Year, y = ~TotalDeaths, type = 'scatter', color = ~ageGroups, mode = "lines+markers") 
+  })
+  
+  output$plot1_2 <- renderPlotly({
+    raceData %>%
+      group_by(race, Year) %>%
+      filter(drugAlcoholInducedCause == "Drug poisonings (overdose) Unintentional (X40-X44)"|
+               drugAlcoholInducedCause == "All other drug-induced causes"|
+               drugAlcoholInducedCause == "Drug poisonings (overdose) Suicide (X60-X64)"|
+               drugAlcoholInducedCause == "Drug poisonings (overdose) Undetermined (Y10-Y14)") %>%
+      summarize(numObs = n(), TotalDeaths = sum(Deaths)) %>%
+      plot_ly(x = ~Year, y = ~TotalDeaths, type = 'scatter', color = ~race, mode = "lines+markers") 
+  })
+  
+  output$plot1_3 <- renderPlotly({
+    genderData %>%
+      group_by(gender, Year) %>%
+      filter(drugAlcoholInducedCause == "Drug poisonings (overdose) Unintentional (X40-X44)"|
+               drugAlcoholInducedCause == "All other drug-induced causes"|
+               drugAlcoholInducedCause == "Drug poisonings (overdose) Suicide (X60-X64)"|
+               drugAlcoholInducedCause == "Drug poisonings (overdose) Undetermined (Y10-Y14)") %>%
+      summarize(numObs = n(), TotalDeaths = sum(Deaths)) %>%
+      plot_ly(x = ~Year, y = ~TotalDeaths, type = 'scatter', color = ~gender, mode = "lines+markers") 
   })
   
   output$plot2_1 <- renderPlotly({
@@ -114,12 +105,85 @@ function(input, output, session){
                ylab("Crude death rate in %"))
   })
   
+  output$map <- renderPlotly({
+    plot_geo(PS, locationmode = 'USA-states') %>%
+      add_trace(
+        z = ~Age_Adjusted_Rate, text = ~hover, locations = ~code,
+        color = ~Age_Adjusted_Rate, colors = 'Purples'
+      ) %>%
+      colorbar(title = "Rate") %>%
+      layout(
+        title = '<br>Age-adjusted and crude death rates <br> <br>',
+        geo = g
+      )
+  })
   
   output$brkdn <- renderPlotly({
     ggplotly(br_down %>%
                filter(Sex==input$Sex,Age_Group==input$Age) %>% 
                ggplot(.,aes(x=Year,y=Crude_Death_Rate))+geom_line()+facet_wrap(~Race) +
                ylab("Crude death rate in %")) 
+  })
+  
+  output$Aftermath <- renderImage({
+    outfile <- tempfile(fileext = '.png')
+    png(outfile, width = 1000, height = 700)
+  })
+  
+  observeEvent({input$beginning1+input$beginning2+input$beginning3+input$beginning4+input$beginning5+input$beginning6+input$beginning7+input$beginning3.5}, {
+    updateTabsetPanel(session, "inTabset",
+                      selected = "tab0")
+  })
+  
+  observeEvent({input$toTab1_0+input$toTab1_1}, {
+    updateTabsetPanel(session, "inTabset",
+                      selected = "tab1")
+  })
+  
+  observeEvent({input$toTab2_1+input$toTab2_2}, {
+    updateTabsetPanel(session, "inTabset",
+                      selected = "tab2")
+  })
+  
+  observeEvent({input$toTab3_1+input$toTab3_2}, {
+    updateTabsetPanel(session, "inTabset",
+                      selected = "tab3")
+  })
+  
+  observeEvent({input$toTab3.5_1+input$toTab3.5_2}, {
+    updateTabsetPanel(session, "inTabset", 
+                      selected = "tab3.5")
+  })
+  
+  
+  observeEvent({input$toTab4_1+input$toTab4_2}, {
+    updateTabsetPanel(session, "inTabset",
+                      selected = "tab4")
+  })
+  
+  observeEvent({input$toTab5_1+input$toTab5_2}, {
+    updateTabsetPanel(session, "inTabset",
+                      selected = "tab5")
+  })
+  
+  observeEvent({input$toTab6_1+input$toTab6_2}, {
+    updateTabsetPanel(session, "inTabset",
+                      selected = "tab6")
+  })
+  
+  observeEvent({input$toTab7_0+input$toTab7_1+input$toTab7_2+input$toTab7_3+input$toTab7_4+input$toTab7_5+input$toTab7_6+input$toTab7_7},{
+    updateTabsetPanel(session, "inTabset", 
+                      selected = "tab7")
+  })
+  
+  output$Consequences <- renderImage({
+    outfile <- tempfile(fileext = '.png')
+    png(outfile, width = 1000, height = 700)
+  })
+  
+  output$Addiction <- renderImage({
+    outfile <- tempfile(fileext = '.png')
+    png(outfile, width = 1000, height = 700)
   })
   
 }   
--- ui.R
+++ ui.R
@@ -5,48 +5,111 @@ library(leaflet)
 library(dplyr)
 library(shinythemes)
 
-navbarPage(title = "The Opioid Crisis in America", id = "inTabset", collapsible = TRUE,  fluid = TRUE,theme = shinytheme("yeti"),
+
+navbarPage(title = "The Opioid Crisis in America", id = "inTabset", collapsible = TRUE,  fluid = TRUE,
            
+           theme = shinytheme("darkly"),
            
            tabPanel(
-             title = "Stratification across age, race & gender", 
+             title = "Picture1", 
+             value = "tab0",
+             column(3,offset=5,actionButton(inputId = "toTab1_0", label = "Next"),
+                    actionButton(inputId = "toTab7_0", label = "End")),
+             br(),br(),
+             img(src='pic1.png', height="600px", width="900px",align="center",
+                 style="display: block; margin-left: auto; margin-right: auto;"),
+             p("© James Nachtwey, TIME Magazine, The Opioid Diaries")
+           ),
+           
+           tabPanel(
+             title = "Crisis Through Time", 
              value = "tab1",
-             sidebarLayout(position = "right",sidebarPanel(h3("Show trends across:"),
-                          width = 3, 
-                          selectInput(inputId = "select",
-                                      label = "Select",
-                                      choices = c("Age","Race","Gender"
-                                      ),selected = "Age")
-             ),mainPanel(tags$h1("Deaths"),
-                         plotlyOutput(outputId = "plot1"),br(),br(),br(),
-                         div(style="display:inline-block;width:100%;margin-left: 500px;",actionButton(inputId = "button1", label = "Next"))))
+             actionButton(inputId = "beginning1", label = "Start"),
+             actionButton(inputId = "toTab2_1", label = "Next"),
+             actionButton(inputId = "toTab7_1", label = "End"),
+             tags$h1("Deaths by Age Group 1999 - 2016"),
+             plotlyOutput(outputId = "plot1"),
+             tags$h1("Deaths by Race 1999-2016"),
+             plotlyOutput(outputId = "plot1_2"),
+             tags$h1("Deaths by Gender 1999-2016"),
+             plotlyOutput(outputId = "plot1_3")
            ),
            
            tabPanel(
-             title = "Worst hit areas",
+             title = "Age Groups Affected", 
+             value = "tab2",
+             actionButton(inputId = "beginning2", label = "Start"),
+             actionButton(inputId = "toTab1_1", label = "Previous"),
+             actionButton(inputId = "toTab3_1", label = "Next"),
+             actionButton(inputId = "toTab7_2", label = "End"),
+             tags$h1("Age Groups By Total Deaths"),
+             sliderInput(inputId = "yearChoice_1", label = "Choose Year to Plot:", min = 1999, max = 2016, value = 1999, width = "500px", animate = TRUE, sep = ""),
+             selectInput(inputId = "input1_1",
+                         label = "Choose Type of Death:",
+                         choices = c("Unintentional" = "Drug poisonings (overdose) Unintentional (X40-X44)",
+                                     "Other" = "All other drug-induced causes",
+                                     "Suicide" = "Drug poisonings (overdose) Suicide (X60-X64)",
+                                     "Undetermined" = "Drug poisonings (overdose) Undetermined (Y10-Y14)")),
+             plotlyOutput(outputId = "plot2_1"),
+             tags$h1("Races By Total Deaths"),
+             sliderInput(inputId = "yearChoice_2", label = "Choose Year to Plot:", min = 1999, max = 2016, value = 1999, width = "500px", animate = TRUE, sep = ""),
+             selectInput(inputId = "input1_2",
+                         label = "Choose Type of Death:",
+                         choices = c("Unintentional" = "Drug poisonings (overdose) Unintentional (X40-X44)",
+                                     "Other" = "All other drug-induced causes",
+                                     "Suicide" = "Drug poisonings (overdose) Suicide (X60-X64)",
+                                     "Undetermined" = "Drug poisonings (overdose) Undetermined (Y10-Y14)")),
+             plotlyOutput(outputId = "plot2_2"),
+             tags$h1("Gender By Total Deaths"),
+             sliderInput(inputId = "yearChoice_3", label = "Choose Year to Plot:", min = 1999, max = 2016, value = 1999, width = "500px", animate = TRUE, sep = ""),
+             selectInput(inputId = "input1_3",
+                         label = "Choose Type of Death:",
+                         choices = c("Unintentional" = "Drug poisonings (overdose) Unintentional (X40-X44)",
+                                     "Other" = "All other drug-induced causes",
+                                     "Suicide" = "Drug poisonings (overdose) Suicide (X60-X64)",
+                                     "Undetermined" = "Drug poisonings (overdose) Undetermined (Y10-Y14)")),
+             plotlyOutput(outputId = "plot2_3")
+           ),
+           
+           tabPanel(
+             title = "Where?",
              value = "tab3",
-             sidebarLayout(position = "right", sidebarPanel(h3("Tweak parameters:"),
-                                                            width=3,
-                                                            sliderInput(inputId = "numObs1", label = "Number of Counties to Plot:", min = 5, max = 3000, value = 1000, width = "850px"),
-                                                            selectInput(inputId = "deathChoice",
-                                                                        label = "Choose type of death:",
-                                                                        choices = c("Unintentional" = "Drug poisonings (overdose) Unintentional (X40-X44)",
-                                                                                    "Other" = "All other drug-induced causes",
-                                                                                    "Suicide" = "Drug poisonings (overdose) Suicide (X60-X64)",
-                                                                                    "Undetermined" = "Drug poisonings (overdose) Undetermined (Y10-Y14)")),
-                                                            sliderInput(inputId = "yearChoice", label = "Choose Year to Plot:", min = 1999, max = 2016, value = 1999, width = "500px", animate = TRUE, sep = "")
-             ),mainPanel(tags$h1("Map of Top Counties Affected By Crisis"),
-                         leafletOutput(outputId = "plot3", width = "100%", height = "450px"),
-                         div(style="display:inline-block;width:100%;margin-left: 1100px;",actionButton(inputId = "button2", label = "Next")),
-                         tags$h1("Table of Top Counties Arranged By Total Deaths"),
-                         dataTableOutput(outputId = "table1")))
+             actionButton(inputId = "beginning3", label = "Start"),
+             actionButton(inputId = "toTab2_2", label = "Previous"),
+             actionButton(inputId = "toTab3.5_1", label = "Next"),
+             actionButton(inputId = "toTab7_3", label = "End"),
+             tags$h1("Map of Top Counties Affected By Crisis"),
+             sliderInput(inputId = "numObs1", label = "Number of Counties to Plot:", min = 5, max = 3000, value = 1000, width = "850px"),
+             selectInput(inputId = "deathChoice",
+                         label = "Choose type of death:",
+                         choices = c("Unintentional" = "Drug poisonings (overdose) Unintentional (X40-X44)",
+                                     "Other" = "All other drug-induced causes",
+                                     "Suicide" = "Drug poisonings (overdose) Suicide (X60-X64)",
+                                     "Undetermined" = "Drug poisonings (overdose) Undetermined (Y10-Y14)")),
+             sliderInput(inputId = "yearChoice", label = "Choose Year to Plot:", min = 1999, max = 2016, value = 1999, width = "500px", animate = TRUE, sep = ""),
+             leafletOutput(outputId = "plot3", width = "100%", height = "600px"),
+             tags$h1("Table of Top Counties Arranged By Total Deaths"),
+             dataTableOutput(outputId = "table1")
            ),
            
            tabPanel(
-             title = "Trends by state", 
+             title = "Picture2", 
+             value = "tab3.5",
+             column(3,offset=5,actionButton(inputId = "beginning3.5", label = "Start"),
+                    actionButton(inputId = "toTab3_2", label = "Previous"),
+                    actionButton(inputId = "toTab4_1", label = "Next"),
+                    actionButton(inputId = "toTab7_4", label = "End")),
+             br(),br(),
+             img(src='pic2.png', height="600px", width="900px",align = "center",
+                 style="display: block; margin-left: auto; margin-right: auto;"),
+             p("© James Nachtwey, TIME Magazine, The Opioid Diaries")
+           ),
+           
+           tabPanel(
+             title = "Looking at States", 
              value = "tab4",
              sidebarLayout(position = "right",
-                           sidebarPanel(h3("Breakdown by state:"),
+                           sidebarPanel(h3("Breakdown by state"),
                                         width = 3, 
                                         selectInput(inputId = "Select State",
                                                     label = "Select State",
@@ -99,12 +162,70 @@ navbarPage(title = "The Opioid Crisis in America", id = "inTabset", collapsible 
                                                                 "Washington",
                                                                 "West Virginia",
                                                                 "Wisconsin",
-                                                                "Wyoming")),p("Click the link below to see the documentation for this app:"),a(href="https://raymondnzaba.com/OpioidCrisis.html","Making sense of the opioid crisis.")
+                                                                "Wyoming"))
                            ),
                            
                            mainPanel(
-                             tags$h1("Crude death rates over time"),
-                             plotlyOutput(outputId = "deathrates"
-                             ),br(),br(),br()
-                           )))
+                             actionButton(inputId = "beginning4", label = "Start"),
+                             actionButton(inputId = "toTab3.5_2", label = "Previous"),
+                             actionButton(inputId = "toTab5_1", label = "Next"),
+                             actionButton(inputId = "toTab7_5", label = "End"),
+                             tags$h1("Crude death rates over time (by state)"),
+                             plotlyOutput(outputId = "deathrates" 
+                             )
+                           ))),
+           
+           tabPanel(
+             title = "Looking at prescribers",
+             value = "tab5",
+             sidebarLayout(position = "right",
+                           sidebarPanel(h3("Breakdown by state"),
+                                        width = 3, strong("Highest opioids prescribers :"),
+                                        p("1. Assistant podiatrics - 55.6%"),
+                                        p("2. Nursing facilities - 23.7 %"), p("3. Cardiovascular technologists - 8%"), 
+                                        p("4. Opticians - 4.7%"),br(),
+                                        strong("States with high average opioid prescriptions :"),
+                                        p("1. Utah - 14.2%"),
+                                        p("2. Tennessee - 12.9%"),
+                                        p("3. Alabama - 11.7%"),
+                                        p("4. Colorado - 11.6%")),
+                           mainPanel(actionButton(inputId = "beginning5", label = "Start"),
+                                     actionButton(inputId = "toTab4_2", label = "Previous"),
+                                     actionButton(inputId = "toTab6_1", label = "Next"),
+                                     actionButton(inputId = "toTab7_6", label = "End"),
+                                     tags$h1("Death rates and prescriptions in 2014"),
+                                     plotlyOutput(outputId = "map",height = "500px" ,width = "800px"))
+             )
+           ),
+           
+           tabPanel(
+             title = "Breakdown",
+             value = "tab6",
+             sidebarLayout(position = "right",
+                           sidebarPanel(h3("Breakdown by state"),
+                                        width = 3,selectInput(inputId = "Sex",
+                                                              label = "Sex",
+                                                              choices = c("Male","Female","Both Sexes")),
+                                        selectInput(inputId = "Age",
+                                                    label = "Age",
+                                                    choices = c("0–14","15–24","25–34","35–44","45–54","55–64","65–74",
+                                                                "75+","All Ages"))),
+                           mainPanel(actionButton(inputId = "beginning6", label = "Start"),
+                                     actionButton(inputId = "toTab5_2", label = "Previous"),
+                                     actionButton(inputId = "toTab7_7", label = "End"),
+                                     tags$h1("Country level statistics"),
+                                     plotlyOutput(outputId = "brkdn")))
+           ),
+           
+           tabPanel(
+             title = "Picture3", 
+             value = "tab7",
+             column(3,offset=5,actionButton(inputId = "beginning7", label = "Start"),
+                    actionButton(inputId = "toTab6_2", label= "Previous")),
+             br(),br(),
+             img(src='pic3.png', height="600px", width="900px",align="center", 
+                 style="display: block; margin-left: auto; margin-right: auto;"),
+             p("© James Nachtwey, TIME Magazine, The Opioid Diaries")
+           )
+           
 )
